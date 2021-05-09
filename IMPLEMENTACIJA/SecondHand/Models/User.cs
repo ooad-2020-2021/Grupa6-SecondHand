de4df1d06975d7debc2770c61a453c691695a663
epanjeta <epanjeta1@etf.usna.ba>
2021-05-09 19:54:40 +0200

    kreirane klase za User

---
+++ IMPLEMENTACIJA/SecondHand/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SecondHand.Models
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public DateTime Birthday { get; set; }
+        public DateTime JoiningDate { get; set; }
+        public string Adress { get; set; }
+        public Gender Gender { get; set; }
+        public string ProfilePicture { get; set; }
+        public Cart Cart { get; set; }
+        public UserProducts UserProducts { get; set; }
+        public PaymentInformation PaymentInformation { get; set; }
+        public double UserRating { get; set; }
+    }
+}
---
+++ IMPLEMENTACIJA/SecondHand/Models/UserProducts.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SecondHand.Models
+{
+    public class UserProducts
+    {
+        public List<Product> Products = new List<Product>();
+
+        public void AddProduct(Product product)
+        {
+            Products.Add(product);
+        }
+        public void DeleteProduct(Product product)
+        {
+            Products.Remove(product);
+        }
+    }
+}
