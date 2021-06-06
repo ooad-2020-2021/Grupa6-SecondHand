73c8b22b67c63b47b37338076fc75f58a43c08e1
epanjeta <epanjeta1@etf.usna.ba>
2021-06-06 14:19:15 +0200

    Uradjeni pogledi Pretraga, PretragaAccessories, PretragaClothing i PretragaShoes. Dodana klasa Filter

---
+++ IMPLEMENTACIJA/SecondHand/Models/Filter.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SecondHand.Models
+{
+    [Keyless]
+    public class Filter
+    {
+        
+        public int ID;
+        public int AccessoriesCategory;
+        public int Brand;
+        public int ClozhingCategory;
+        public int ClothingSize;
+        public int Color;
+        public int Condition;
+        public int Gender;
+        public int Material;
+        public int ShoesCategory;
+    }
+}
